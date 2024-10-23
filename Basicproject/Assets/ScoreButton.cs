9e08a621c56bcaa251ab3cacfeaa7e4c7e03af68
Joyoounjin <dbswls060@gmail.com>
2024-10-23 12:38:39 +0900

    Q2
    - 구현사항 1,2 완료

---
+++ Basicproject/Assets/ScoreButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//public class ScoreButton : MonoBehaviour
+//{
+//    public Button scoreButton;
+//    public int score = 0;
+//    void Awake()
+//    {
+//        scoreButton = GetComponent<Button>();
+//        scoreButton.onClick.AddListener(PointUp); 
+//    }
+
+//    void PointUp()
+//    {
+//        score++;
+//    }
+//}
---
+++ Basicproject/Assets/ScoreText.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public Button scoreButton;
+    public int score = 0;
+
+    private void Awake()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        scoreButton.onClick.AddListener(RefreshUI);
+    }
+
+    void RefreshUI()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+}
